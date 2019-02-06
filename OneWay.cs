3fe7d50f4c88e8f865a9d03c949ee07e6be3608c
hamstar0 <webutek@gmail.com>
2019-02-06 05:07:02 -0800

    v1.0.1 * Improved backtracking check via player position rather than screen position

--- OneWay.cs
+++ OneWay.cs
@@ -12,6 +12,7 @@ namespace OneWay {
 
 
 	class OneWayPlayer : ModPlayer {
+		private Vector2 PrevScrPos = default( Vector2 );
 		private Vector2 PrevPos1 = default( Vector2 );
 		private Vector2 PrevPos2 = default( Vector2 );
 
@@ -25,22 +26,25 @@ namespace OneWay {
 		////////////////
 
 		public override void ModifyScreenPosition() {
+			var plrPos = this.player.position;
+
 			if( this.PrevPos2 == default(Vector2) ) {
-				if( this.PrevPos1 != Main.screenPosition ) {
+				if( this.PrevPos1 != plrPos ) {
 					this.PrevPos2 = this.PrevPos1;
-					this.PrevPos1 = Main.screenPosition;
+					this.PrevPos1 = plrPos;
 				}
 				return;
 			}
 
-			float prev1Dist = Vector2.Distance( this.PrevPos1, Main.screenPosition );
-			float prev2Dist = Vector2.Distance( this.PrevPos2, Main.screenPosition );
+			float prev1Dist = Vector2.Distance( this.PrevPos1, plrPos );
+			float prev2Dist = Vector2.Distance( this.PrevPos2, plrPos );
 
 			if( prev1Dist >= prev2Dist ) {
-				Main.screenPosition = this.PrevPos1;
-			} else if( this.PrevPos1 != Main.screenPosition ) {
+				Main.screenPosition = this.PrevScrPos;
+			} else if( this.PrevPos1 != plrPos ) {
 				this.PrevPos2 = this.PrevPos1;
-				this.PrevPos1 = Main.screenPosition;
+				this.PrevPos1 = plrPos;
+				this.PrevScrPos = Main.screenPosition;
 			}
 		}
 	}
